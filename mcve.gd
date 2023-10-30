3e0362209317bddc3954f02dc9f668de37e66b7c
fuzhou.chen <fuzhouch@gmail.com>
2023-10-30 15:05:45 +0800

    [NEW] Add minimal example from Guilherme Nascimento.

---
+++ mcve.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+func _process(delta):
+	prints("right_physical_or_unicode:", Input.is_action_pressed("right_physical_or_unicode"))
+	prints("d-pad-right:", Input.is_action_pressed("d-pad-right"))
+	prints("axis-0-plus:", Input.is_action_pressed("axis-0-plus"))
